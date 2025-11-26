0d022b3f54d1cd5223f9b63f6905098f1481b055
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-11-25 22:48:21 -0300

    feat: Cria estrutura de painel de controle e roteamento inicial

--- fintrackr-web/src/app/app.routes.ts
+++ fintrackr-web/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  // futuras rotas:
+  // { path: 'auth', loadComponent: () => import('./features/auth/xxx').then(m => m.AuthComponent) }
+];
