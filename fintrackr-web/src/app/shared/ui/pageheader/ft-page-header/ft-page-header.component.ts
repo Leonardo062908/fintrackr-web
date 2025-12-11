3c563c19646fe1bc98ed15b665659f8d97c44f79
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-12-11 08:50:09 -0300

    feat: creates a feature for the header components

---
+++ fintrackr-web/src/app/shared/ui/pageheader/ft-page-header/ft-page-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ft-page-header',
+  imports: [],
+  templateUrl: './ft-page-header.component.html',
+  styleUrl: './ft-page-header.component.scss'
+})
+export class FtPageHeaderComponent {
+
+}
