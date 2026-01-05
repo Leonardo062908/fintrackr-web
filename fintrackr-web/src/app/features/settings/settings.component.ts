c7d04ccfcbb17f567e032509bc8c1914cacc9a6e
Leonardo062908 <leonardomatheus9155@gmail.com>
2026-01-05 19:05:34 -0300

    feat: creating a base for the settings screen

--- fintrackr-web/src/app/features/settings/settings.component.ts
+++ fintrackr-web/src/app/features/settings/settings.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { FtPageHeaderComponent } from '../../shared/ui/pageheader/ft-page-header/ft-page-header.component';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [FtPageHeaderComponent],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {
-
-}
+export class SettingsComponent {}
