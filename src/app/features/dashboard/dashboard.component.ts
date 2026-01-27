7c7ba5c96003c0916b65de0b116feec137bac4f6
Leonardo062908 <leonardomatheus9155@gmail.com>
2026-01-26 23:50:10 -0300

    fix: Refactor the Dashboard to use shared components.

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FtCardComponent } from '../../shared/ui/cards/ft-card.component';
+import { FtPageHeaderComponent } from '../../shared/ui/pageheader/ft-page-header/ft-page-header.component';
 
 @Component({
   selector: 'app-dashboard',
