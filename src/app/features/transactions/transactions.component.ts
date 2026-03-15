d3d2dfe6646bea1124b5333519c021c3d489ad1e
Leonardo062908 <leonardomatheus9155@gmail.com>
2026-03-15 19:11:12 -0300

    fix: the transacations page retrieves the list from 'TransactonsService' an exports it to the template

--- src/app/features/transactions/transactions.component.ts
+++ src/app/features/transactions/transactions.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FtPageHeaderComponent } from '../../shared/ui/pageheader/ft-page-header/ft-page-header.component';
+import { TransactionsService } from '../../core/service/transactions.service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import {
+  Transaction,
+  TransactionType,
+} from '../../core/models/transaction.model';
 
 @Component({
   selector: 'app-transactions',
-  imports: [FtPageHeaderComponent],
+  imports: [FtPageHeaderComponent, CommonModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss',
 })
-export class TransactionsComponent {}
+export class TransactionsComponent implements OnInit {
+  transactions$!: Observable<Transaction[]>;
+
+  constructor(private transactionsService: TransactionsService) {}
+
+  ngOnInit(): void {
+    this.transactions$ = this.transactionsService.getTransactions();
+  }
+}
