ac155f8e6c03cfbe27fb497097b4f0d3bc6dbc08
Leonardo062908 <leonardomatheus9155@gmail.com>
2026-03-15 21:07:33 -0300

    fix: refactor the trasacation page and connection to the transaction service

--- src/app/features/transactions/transactions.component.ts
+++ src/app/features/transactions/transactions.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FtPageHeaderComponent } from '../../shared/ui/pageheader/ft-page-header/ft-page-header.component';
-import { TransactionsService } from '../../core/service/transactions.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import {
   Transaction,
   TransactionType,
 } from '../../core/models/transaction.model';
+import { TransactionsService } from '../../core/service/transactions.service';
+import { FtPageHeaderComponent } from '../../shared/ui/pageheader/ft-page-header/ft-page-header.component';
 
 @Component({
   selector: 'app-transactions',
-  imports: [FtPageHeaderComponent, CommonModule],
+  imports: [FtPageHeaderComponent, CommonModule, RouterLink],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss',
 })
 export class TransactionsComponent implements OnInit {
   transactions$!: Observable<Transaction[]>;
+
+  readonly transactionTypeIncome = TransactionType.Income;
+  readonly transactionTypeExpense = TransactionType.Expense;
 
   constructor(private transactionsService: TransactionsService) {}
 
